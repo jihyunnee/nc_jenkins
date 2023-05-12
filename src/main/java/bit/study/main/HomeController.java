c7c90e81707f1c5080a1b6eb0ba5cf34e6320c85
jihyunnee <ldlwlgus0076@gmail.com>
2023-05-12 14:03:38 +0900

    0512

---
+++ src/main/java/bit/study/main/HomeController.java
@@ -0,0 +1,17 @@
+package bit.study.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	
+	    @GetMapping("/")
+	    public String home(){
+	        return "home";
+	    }
+	
+
+}
